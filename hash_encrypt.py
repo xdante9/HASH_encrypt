afd4606483b3360dbff3c7b9b75e384c19893a2b
xdante9 <48583061+xdante9@users.noreply.github.com>
2019-03-15 00:42:49 -0700

    Add files via upload

---
+++ hash_encrypt.py
@@ -0,0 +1,28 @@
+import hashlib #import lib
+
+while True:
+        hash = (raw_input("select your hash :"))
+        if hash == 'sha1':
+                sha1_encrypt = (raw_input("sha1 encrypt = ")) #input string
+                print "[",hashlib.sha1(sha1_encrypt).hexdigest(),"]"#output md5 encrypt
+        if hash == 'sha224':
+                sha224_encrypt = (raw_input("sha224 encrypt = ")) #input string
+                print "[",hashlib.sha224(sha224_encrypt).hexdigest(),"]" #output sha224 encrypt
+        if hash == 'sha384':
+                sha384_encrypt = (raw_input("sha384_encrypt = "))
+                print "[",hashlib.sha384(sha384_encrypt).hexdigest(),"]"
+        if hash == 'sha256':
+                sha256_encrypt = (raw_input("sha256 encrypt = "))
+                print "[",hashlib.sha256(sha256_encrypt).hexdigest(),"]"
+        if hash == 'sha512':
+                sha512_encrypt = (raw_input("sha512 encrypt = "))
+                print "[",hashlib.sha512(sha512_encrypt).hexdigest(),"]"
+        if hash == 'md5':
+                md5_encrypt = (raw_input("md5 encrypt = "))
+                print "[",hashlib.md5(md5_encrypt).hexdigest(),"]"
+                
+
+
+        exit = raw_input("1.continue,2.exit = ")
+        if exit == '2':
+                break
