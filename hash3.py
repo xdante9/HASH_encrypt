570b9797b3349fc9a109c2d4c70426fc84df7a57
xdante9 <48583061+xdante9@users.noreply.github.com>
2020-06-22 01:20:38 +0800

    Update hash3.py

--- hash3.py
+++ hash3.py
@@ -14,34 +14,34 @@ select hash: '''))
         if hash == 'sha1':
                 sha1_encrypt = (input("sha1 encrypt = ")) #input string
                 print ("[",hashlib.sha1(sha1_encrypt.encode('utf-8')).hexdigest(),"]")#output md5 encrypt
-        if hash == 'sha224':
+        elif hash == 'sha224':
                 sha224_encrypt = (input("sha224 encrypt = ")) #input string
                 print ("[",hashlib.sha224(sha224_encrypt.encode('utf-8')).hexdigest(),"]") #output sha224 encrypt
-        if hash == 'sha384':
+        elif hash == 'sha384':
                 sha384_encrypt = (input("sha384_encrypt = "))
                 print ("[",hashlib.sha384(sha384_encrypt.encode('utf-8')).hexdigest(),"]")
-        if hash == 'sha256':
+        elif hash == 'sha256':
                 sha256_encrypt = (input("sha256 encrypt = "))
                 print ("[",hashlib.sha256(sha256_encrypt.encode('utf-8')).hexdigest(),"]")
-        if hash == 'sha512':
+        elif hash == 'sha512':
                 sha512_encrypt = (input("sha512 encrypt = "))
                 print ("[",hashlib.sha512(sha512_encrypt.encode('utf-8')).hexdigest(),"]")
-        if hash == 'md5':
+        elif hash == 'md5':
                 md5_encrypt = (input("md5 encrypt = "))
                 print ("[",hashlib.md5(md5_encrypt.encode('utf-8')).hexdigest(),"]")
-        if hash == 'blake2b':
+        elif hash == 'blake2b':
                 blake2b_encrypt = (input("blake2b encrypt = "))
                 print ("[",hashlib.blake2b(blake2b_encrypt.encode('utf-8')).hexdigest(),"]")
-        if hash == 'blake2s':
+        elif hash == 'blake2s':
                 blake2s_encrypt = (input("blake2s encrypt = "))
                 print ("[",hashlib.blake2s(blake2s_encrypt.encode('utf-8')).hexdigest(),"]")
-        if hash == 'sha3_224':
+        elif hash == 'sha3_224':
                 sha3_224_encrypt = (input("sha3_224 encrypt = "))
                 print ("[",hashlib.sha3_224(sha3_224_encrypt.encode('utf-8')).hexdigest(),"]")
-        if hash == 'sha3_256':
+        elif hash == 'sha3_256':
                 sha3_256_encrypt = (input("sha3_256 encrypt = "))
                 print ("[",hashlib.sha3_256(sha3_256_encrypt.encode('utf-8')).hexdigest(),"]")
-        if hash == 'sha3_512':
+        elif hash == 'sha3_512':
                 sha3_512_encrypt = (input("sha3_512 encrypt = "))
                 print ("[",hashlib.sha3_512(sha3_512_encrypt.encode('utf-8')).hexdigest(),"]")
         else:
