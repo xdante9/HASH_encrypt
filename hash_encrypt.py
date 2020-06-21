961787729e84b38f853bd4c26dfd315de3eaba56
xdante9 <48583061+xdante9@users.noreply.github.com>
2020-06-22 01:22:46 +0800

    Update hash_encrypt.py

--- hash_encrypt.py
+++ hash_encrypt.py
@@ -11,22 +11,23 @@ while True:
         if hash == 'sha1':
                 sha1_encrypt = (raw_input("sha1 encrypt = ")) #input string
                 print ">|>","[",hashlib.sha1(sha1_encrypt).hexdigest(),"]"#output md5 encrypt
-        if hash == 'sha224':
+        elif hash == 'sha224':
                 sha224_encrypt = (raw_input("sha224 encrypt = ")) #input string
                 print ">|>","[",hashlib.sha224(sha224_encrypt).hexdigest(),"]" #output sha224 encrypt
-        if hash == 'sha384':
+        elif hash == 'sha384':
                 sha384_encrypt = (raw_input("sha384_encrypt = "))
                 print ">|>","[",hashlib.sha384(sha384_encrypt).hexdigest(),"]"
-        if hash == 'sha256':
+        elif hash == 'sha256':
                 sha256_encrypt = (raw_input("sha256 encrypt = "))
                 print ">|>","[",hashlib.sha256(sha256_encrypt).hexdigest(),"]"
-        if hash == 'sha512':
+        elif hash == 'sha512':
                 sha512_encrypt = (raw_input("sha512 encrypt = "))
                 print ">|>","[",hashlib.sha512(sha512_encrypt).hexdigest(),"]"
-        if hash == 'md5':
+        elif hash == 'md5':
                 md5_encrypt = (raw_input("md5 encrypt = "))
                 print ">|>","[",hashlib.md5(md5_encrypt).hexdigest(),"]"
-                
+        else:
+                print("Yang Benar dong Sayangg")
         exit = raw_input('''|--------<<<<<[]>>>>>--------|
 [ 1.restart ]
 [ 2.exit ]
