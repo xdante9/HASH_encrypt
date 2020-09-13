963bb1aebc6001da0167dee28e2b17bba74cab09
xdante9 <48583061+xdante9@users.noreply.github.com>
2020-09-14 00:07:42 +0800

    Add files via upload

--- hash3.py
+++ hash3.py
@@ -8,8 +8,8 @@ try:
                 hash = (input('''|-------------<<<<<[]>>>>>-------------|
 |    [   sha1    ]       [  blake2s  ] |
 |    [   sha224  ]       [ shake_128 ] |
-|    [  sha384   ]       [ shake_256 ] |
-|    [   sha256  ]       [ sha3_224  ] |
+|    [  sha256   ]       [ shake_256 ] |
+|    [   sha384  ]       [ sha3_224  ] |
 |    [  sha512   ]       [ sha3_256  ] |
 |    [    md5    ]       [ sha3_512  ] |
 |    [  blake2b  ]                     |
@@ -23,33 +23,42 @@ select hash: '''))
                         sha224_encrypt = (input("sha224 encrypt = ")) #input string
                         sha224result = hashlib.sha224(sha224_encrypt.encode('utf-8')).hexdigest() #output sha224 encrypt
                         print ("[", sha224result, "]")
-                elif hash == 'sha384':
-                        sha384_encrypt = (input("sha384_encrypt = "))
-                        print ("[",hashlib.sha384(sha384_encrypt.encode('utf-8')).hexdigest(),"]")
                 elif hash == 'sha256':
-                        sha256_encrypt = (input("sha256 encrypt = "))
-                        print ("[",hashlib.sha256(sha256_encrypt.encode('utf-8')).hexdigest(),"]")
+                        sha256_encrypt = (input("sha256_encrypt = "))
+                        sha256result = hashlib.sha256(sha256_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", sha256result, "]")
+                elif hash == 'sha384':
+                        sha384_encrypt = (input("sha384 encrypt = "))
+                        sha384result = hashlib.sha384(sha384_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", sha384result, "]")
                 elif hash == 'sha512':
                         sha512_encrypt = (input("sha512 encrypt = "))
-                        print ("[",hashlib.sha512(sha512_encrypt.encode('utf-8')).hexdigest(),"]")
+                        sha512result = hashlib.sha512(sha512_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", sha512result, "]")
                 elif hash == 'md5':
                         md5_encrypt = (input("md5 encrypt = "))
-                        print ("[",hashlib.md5(md5_encrypt.encode('utf-8')).hexdigest(),"]")
+                        md5result = hashlib.md5(md5_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", md5result, "]")
                 elif hash == 'blake2b':
                         blake2b_encrypt = (input("blake2b encrypt = "))
-                        print ("[",hashlib.blake2b(blake2b_encrypt.encode('utf-8')).hexdigest(),"]")
+                        blake2bresult = hashlib.blake2b(blake2b_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", blake2bresult, "]")
                 elif hash == 'blake2s':
                         blake2s_encrypt = (input("blake2s encrypt = "))
-                        print ("[",hashlib.blake2s(blake2s_encrypt.encode('utf-8')).hexdigest(),"]")
+                        blake2sresult = hashlib.blake2s(blake2s_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", blake2sresult, "]")
                 elif hash == 'sha3_224':
                         sha3_224_encrypt = (input("sha3_224 encrypt = "))
-                        print ("[",hashlib.sha3_224(sha3_224_encrypt.encode('utf-8')).hexdigest(),"]")
+                        sha3_224result = hashlib.sha3_224(sha3_224_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", sha3_224result, "]")
                 elif hash == 'sha3_256':
                         sha3_256_encrypt = (input("sha3_256 encrypt = "))
-                        print ("[",hashlib.sha3_256(sha3_256_encrypt.encode('utf-8')).hexdigest(),"]")
+                        sha3_256result = hashlib.sha3_256(sha3_256_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", sha3_256result,"]")
                 elif hash == 'sha3_512':
                         sha3_512_encrypt = (input("sha3_512 encrypt = "))
-                        print ("[",hashlib.sha3_512(sha3_512_encrypt.encode('utf-8')).hexdigest(),"]")
+                        sha3_512result = hashlib.sha3_512(sha3_512_encrypt.encode('utf-8')).hexdigest()
+                        print ("[", sha3_512, "]")
                 else:
                         print("Nggak Ada Sayanggg")
 
@@ -64,11 +73,84 @@ select hash: '''))
                         clear = lambda: os.system('clear')
                         clear()
                 if exit == '2':
-                        encryptresult = open('encrypt-hash.txt', 'a+')
-                        encryptresult.write("[ " + str(sha1result) + " ]" + '\n')
-                        encryptresult.close()
-                        clear = lambda: os.system('clear')
-                        clear()
+                        if hash == 'sha1':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[   sha1    ]" + " >\> " + "[ " + str(sha1result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'sha224':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[   sha224  ]" + " >\> " + "[ " + str(sha224result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'sha256':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[  sha256   ]" + " >\> " + "[ " + str(sha256result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'sha384':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[   sha384  ]" + " >\> " + "[ " + str(sha384result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'sha512':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[  sha512   ]" + " >\> " + "[ " + str(sha512result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'md5':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[    md5    ]" + " >\> " + "[ " + str(md5result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'blake2b':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[  blake2b  ]" + " >\> " + "[ " + str(blake2bresult) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'blake2s':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[  blake2s  ]" + " >\> " + "[ " + str(blake2sresult) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'shake_128':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[ shake_128 ]" + " >\> " + "[ " + str(shake_128result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'shake_256':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[ shake_256 ]" + " >\> " + "[ " + str(shake_256result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'sha3_224':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[ sha3_224  ]" + " >\> " + "[ " + str(sha3_224result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'sha3_256':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[ sha3_256  ]" + " >\> " + "[ " + str(sha3_256result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
+                        if hash == 'sha3_512':
+                                encryptresult = open('encrypt-hash.txt', 'a+')
+                                encryptresult.write("[ sha3_512  ]" + " >\> " + "[ " + str(sha3_512result) + " ]" + '\n')
+                                encryptresult.close()
+                                clear = lambda: os.system('clear')
+                                clear()
                 if exit == '3':
                         clear = lambda: os.system('clear')
                         clear()
